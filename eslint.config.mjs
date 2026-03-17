79b5486835e581bd8553204ed1b0d3a79a380e6c
Daan Smit <69450888+Daan-Smit@users.noreply.github.com>
2026-03-17 09:15:05 +0100

    Update eslint.config.mjs
    Co-authored-by: ant-george <140204491+ant-george@users.noreply.github.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
     rules: {
-        'no-console': 'warn',                   // voorkomt vergeten console.logs
+        'no-console': 'warn',                   // prevents forgotten console.logs
         'no-unused-vars': 'error',              // houdt code schoon
         'vue/require-default-prop': 'error',    // betere component documentatie
     }}
