8a8ab79d6cd224d1676e4b7bc2935a394a340808
Anthony Ross <agross@live.nl>
2026-03-31 07:56:47 +0200

    Aanmaken gemaakt. Daarnaast routing voor projecten aangepast, zodat het uitkomt volgens de user stories.

--- app/pages/projecten.vue
+++ app/pages/projecten.vue
@@ -1,16 +1,4 @@
-<script setup lang="ts">
-const { projects, loading, error, fetchProjects } = useProjects();
-
-await fetchProjects();
-</script>
-
 <template>
-  <div>
-    <h1 class="mb-6">Projecten</h1>
-    <ProjectGrid
-        :projects="projects"
-        :loading="loading"
-        :error="error"
-    />
-  </div>
+  <NuxtPage />
 </template>
+
